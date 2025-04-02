66e81e9262d10fc28a10f6b302980902b5fc9141
Houda-jebari <132141535+Lenesli@users.noreply.github.com>
2025-04-02 23:37:11 +0000

    Adding components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from "./Pages/HomePage";
+import SignUpPage from './Pages/SignUpPage';
+import LoginPage from './Pages/LoginPage';
+import SettingsPage from './Pages/SettingsPage';
+import ProfilePage from './Pages/ProfilePage';
+import { useAuthStore } from './store/useAuthStore';
 
 const App = () => {
+  const {authUser,checkAuth} = useAuthStore;
+    useEffect(()=>{
+      checkAuth();
+    },[checkAuth]);
+    console.log({authUser});
   return (
-    <div className='text-red-500'>
-      helloo 
+    <div >
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/settings" element={<SettingsPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,24 @@
+// different functions and states that we can use in different components
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create( (set)=>({
+    authUser : null , 
+    isSigningUp:false,
+    isLoginIn:false , 
+    isUpdatingProfile:false,
+    isCheckingAuth:true,
+
+
+    checkAuth : async() =>{
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({authUser: res.data});
+        } catch (error) {
+            console.log("Error in checkAuth: " , error);
+            set({authUser : null});
+        }finally{
+            set({isCheckingAuth:false});
+        }
+    }
+}))
