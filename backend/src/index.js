ad4ecf2eac6d4a1eab090c8a09ae0f3566961255
Houda-jebari <132141535+Lenesli@users.noreply.github.com>
2025-04-06 23:50:57 +0100

    Adding code

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv"; // Imports the dotenv package. dotenv is used to load environment variables from a .env file into process.env
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -13,6 +14,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json())
 app.use(cookieParser());
+app.use(cors(
+    {
+        origin : "http://localhost:5173",
+        credentials :true , //allow cookies
+    }
+));
 app.use("/api/auth" , authRoutes);
 app.use("/api/messages" , messageRoutes);
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
@@ -9,4 +10,6 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+
+  
 }
